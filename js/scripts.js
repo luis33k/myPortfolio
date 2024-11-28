b697207612295f30c14ad9f326b3c66360d7d453
Luis G. <luisgornes@hotmail.com>
2024-11-28 16:50:06 -0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const logo = document.querySelector(".dropdown-logo");
-    menu.classList.toggle("open");
-    logo.classList.toggle("open");
-  }
